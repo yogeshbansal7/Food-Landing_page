a128b0194f20629fd5c126d9f5a5fe614eb1e64b
Yogesh <ybansal961@gmail.com>
2023-04-28 01:33:54 +0530

    Work section completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Home from "./components/Home";
 import About from './components/About';
+import Work from './components/Work';
 
 function App() {
   return (
     <div className="App">
       <Home />
       <About />
+      <Work />
     </div>
   );
 }
