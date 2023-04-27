9689adbe87c6ecae3dd3da418f89d684872e5732
Yogesh <ybansal961@gmail.com>
2023-04-28 01:53:40 +0530

    Footer is added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from './components/About';
 import Work from './components/Work';
 import Testimonials from './components/Testimonials';
 import Contact from './components/Contact';
+import Footer from './components/Footer'
 
 function App() {
   return (
