d15c7bd74f8adc5d7552236d7a25441f7deda4d0
Yogesh <ybansal961@gmail.com>
2023-04-28 01:10:38 +0530

    About sectiom completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
-import Home from "./components/Home"
+import Home from "./components/Home";
+import About from './components/About';
 
 function App() {
   return (
     <div className="App">
       <Home />
+      <About />
     </div>
   );
 }
